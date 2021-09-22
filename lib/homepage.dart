8be81a393b850ac231baa35e424c6d8d25c7c6c0
Irtaza Fayaz <35741497+irtazafayaz@users.noreply.github.com>
2021-09-23 00:11:30 +0500

    Initial Commit

---
+++ lib/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Text("What is life? if not full of tensions, anxieties, hardships"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
